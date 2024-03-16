9fe7fdbb37b7702d5750525ecdd2cedc6ca0fe14
Sheela <91262191+123sheela@users.noreply.github.com>
2024-03-16 23:03:18 +0530

    Add files via upload

---
+++ Part-2/Day-16/Objects.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Objects {
+    int a= 15;
+  }
+  class Second {
+    public static void main(String[] args) {
+      Objects myObj = new Objects();
+      System.out.println(myObj.a);
+    }
+  }
